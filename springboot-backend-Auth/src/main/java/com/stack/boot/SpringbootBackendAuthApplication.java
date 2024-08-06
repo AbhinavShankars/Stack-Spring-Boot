815543e5e5bb33adcb00b426005a9e683a9a6415
AbhinavShankars <abhinavshankars@rediffmail.com>
2024-08-06 09:50:06 +0530

    Creating Auth token and resource server

---
+++ springboot-backend-Auth/src/main/java/com/stack/boot/SpringbootBackendAuthApplication.java
@@ -0,0 +1,13 @@
+package com.stack.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendAuthApplication.class, args);
+	}
+
+}
---
+++ springboot-backend-Auth/src/main/java/com/stack/boot/security/Authorities.java
@@ -0,0 +1,9 @@
+package com.stack.boot.security;
+
+public enum Authorities {
+
+    ROLE_ANONYMOUS,
+    ROLE_USER,
+    ROLE_ADMIN
+
+}
